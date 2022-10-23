a848d77e31cb2267dede5651206ab8c5e1aee2a0
ps777 <56358194+piotrszubert@users.noreply.github.com>
2022-10-23 16:18:50 +0200

    added custom theme

--- src/lib/ThemeSelect.svelte
+++ src/lib/ThemeSelect.svelte
@@ -1,6 +1,6 @@
 <script>
     import { onMount } from 'svelte';
-    const themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"]
+    const themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", 'mytheme']
     
     onMount(() => {
         const root = document.documentElement;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,32 @@ module.exports = {
   theme: {
     extend: {},
   },
-  // plugins: [],
+  daisyui: {
+    themes: [
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",
+      {
+        mytheme: {
+        "primary": "#a855f7",
+                
+        "secondary": "#c4b5fd",
+                
+        "accent": "#f3f4f6",
+                
+        "neutral": "#292524",
+                
+        "base-100": "#c084fc",
+                
+        "info": "#1d4ed8",
+                
+        "success": "#a3e635",
+                
+        "warning": "#fcd34d",
+                
+        "error": "#fda4af",
+                },
+              },
+            ],
+          },
   plugins: [require("daisyui")],
 }
+
